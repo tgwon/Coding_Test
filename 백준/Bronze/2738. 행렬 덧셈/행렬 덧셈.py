50367f543237213a95a67cf49aaa0322c714da1c
ytg000629 <102985590+ytg000629@users.noreply.github.com>
2023-03-11 16:02:18 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,15 @@
+N,M = map(int,input().split())
+A =[]
+B= []
+for i in range(N):
+    A.append(list(map(int,input().split())))
+for i in range(N):
+    B.append(list(map(int,input().split())))
+C =[]
+for i in range(N):
+    for j in range(M):
+        C.append(A[i][j] + B[i][j])
+for i in range(N):
+    for j in range(M):
+        r = C[0+(i*M):M*(i+1)]
+    print(*r)
