4cf53c6f7917f109890629d4b6b7473cedc977db
tgwon <102985590+ytg000629@users.noreply.github.com>
2023-04-11 22:12:28 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,9 +1,4 @@
-S = input()
-list = []
-for i in range(len(S)):
-    list.append(S[i])
+a = input()
+
 for i in range(26):
-    try:
-        print(list.index(chr(97+i)), end=' ')
-    except:
-        print(-1, end=' ')
+    print(a.find(chr(97+i)), end = ' ')
