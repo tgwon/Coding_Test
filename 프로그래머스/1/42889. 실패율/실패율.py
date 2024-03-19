1a5d845f48f8b61818be3268d5a91e188610e93f
tgwon <102985590+tgwon@users.noreply.github.com>
2024-03-19 17:54:00 +0900

    [level 1] Title: 실패율, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,12 +1,12 @@
 def solution(N, stages):
-    fail = [0 for _ in range(N+1)]
-
-    for stage in stages:
-        fail[ stage - 1 ] += 1
-        
-    for i in range(len(fail)):
-        if sum(fail[i:]) != 0:
-            fail[i] /= sum(fail[i:])
-        
-    return [i + 1 for i in sorted(range(len(fail)), key=lambda k: fail[k],reverse=True) if i < N]
-
+    result = []
+    stages.sort()
+    for i in range(1,N+1):
+        try:
+            fail_rate = stages.count(i)/len(stages[stages.index(i):])
+        except:
+            fail_rate = 0
+        result.append((i,fail_rate))
+    result.sort(key = lambda x: x[1], reverse=True)
+    return [i for i,k in result]
+solution(5, [2,1,2,6,2,5,3,3])
