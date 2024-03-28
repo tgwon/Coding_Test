d5cd7a46b8f56d0c60b438ba8b3d35668a5a08b4
tgwon <102985590+tgwon@users.noreply.github.com>
2024-03-28 20:21:09 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -0,0 +1,28 @@
+N, M = map(int, input().split())
+original = []
+count = []
+
+for _ in range(N):
+    original.append(input())
+
+for a in range(N-7):
+    for b in range(M-7):
+        # w로 시작할 경우 바뀐 체스 판의 갯수 
+        index1 = 0
+        # b로 시작할 경우 바뀐 체스 판의 갯수
+        index2 = 0
+        for i in range(a, a+8):
+            for j in range(b, b+8):
+                if (i+j) % 2 == 0:
+                    if original[i][j] != 'W':
+                        index1 += 1
+                    if original[i][j] != 'B':
+                        index2 += 1
+                else:
+                    if original[i][j] != 'B':
+                        index1 += 1
+                    if original[i][j] != 'W':
+                        index2 += 1
+        count.append(min(index1, index2))
+
+print(min(count))
