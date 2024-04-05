9b35c05fbdb76ee3f2574b77c6bdafa72f6a9871
tgwon <102985590+tgwon@users.noreply.github.com>
2024-04-06 03:24:51 +0900

    [Silver III] Title: 통계학, Time: 492 ms, Memory: 53636 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -0,0 +1,29 @@
+from collections import Counter
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+result = []
+
+for i in range(n):
+    result.append(int(input()))
+    
+result.sort()
+
+print(round(sum(result)/len(result)))
+print(result[len(result)//2])
+
+result2 = []
+max_value = max(Counter(result).values())
+
+for i,j in Counter(result).items():
+    if j == max_value:
+        result2.append(i)
+        
+if len(result2) == 1:
+    print(result2[0])
+else:
+    print(result2[1])
+    
+print(max(result) - min(result))
