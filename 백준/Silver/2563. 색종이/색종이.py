3a845f5b24d02887c0f7c956b103125dc2e2a32e
tgwon <102985590+tgwon@users.noreply.github.com>
2024-04-12 01:07:04 +0900

    [Silver V] Title: 색종이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.py
+++ 백준/Silver/2563. 색종이/색종이.py
@@ -1,21 +1,15 @@
-n = int(input())
-map_array = [[0]*100 for _ in range(100)] # 100x100 도화지 만들기
+import sys
+input = sys.stdin.readline
 
-for _ in range(n):
-    x, y = map(int, input().split())
-    # x : x ~ x+10
-    # y : y ~ y+10
-    for i in range(y, y+10):
-        idx_y = i
-        # i,j에 해당하는 좌표에 점을 찍음
-        for j in range(x, x+10):
-            idx_x = j
-            if map_array[idx_y][idx_x] == 0:
-                map_array[idx_y][idx_x] = 1
-            else:
-                pass
-            
-sum_array = 0
-for i in range(100):
-    sum_array += sum(map_array[i])
-print(sum_array)
+paper = [[0]*100 for i in range(100)]
+
+for _ in range(int(input())):
+    a,b = map(int,input().split())
+    for i in range(10):
+        for j in range(10):
+            paper[a+i][b+j] = 1
+
+r = 0
+for i in paper:
+    r += sum(i)
+print(r)
